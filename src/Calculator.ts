1bc0331cc047b77b13d5c6be74c2fb2c756a2953
Leto <leto4100@gmail.com>
2022-04-01 14:54:52 +0100

    first commit

---
+++ src/Calculator.ts
@@ -0,0 +1,36 @@
+export class Calculator {
+  public processRawInputReturnSum (rawInput: string): number {
+    const input: number[] = this.processRawInput(rawInput)
+    return this.sum(input)
+  }
+
+  private processRawInput (rawInput: string): number[] {
+    const [delimiter, input] = this.processDelimiter(rawInput)
+    const inputCol = input.split(delimiter)
+    return inputCol
+      .map(element => parseInt(element))
+      .filter(element => !isNaN(element))
+  }
+
+  private sum (input: number[]): number {
+    const defaultValue = 0
+    if (input.some(element => element < 0)) {
+      throw new Error('Negatives not allowed')
+    }
+    return input.reduce((accumulator, currentValue) => {
+      if (currentValue <= 1000) {
+        accumulator += currentValue
+      }
+      return accumulator
+    }, defaultValue)
+  }
+
+  private processDelimiter (rawInput: string): [string|RegExp, string] {
+    let delimiter: string|RegExp = /[,\n]/
+    if (rawInput.startsWith('//')) {
+      delimiter = rawInput.substring(2, rawInput.indexOf('\n'))
+      rawInput = rawInput.slice(rawInput.indexOf('\n'))
+    }
+    return [delimiter, rawInput]
+  }
+}
---
+++ src/__tests__/Calculator.ts
@@ -0,0 +1,58 @@
+import { Calculator } from '../Calculator'
+
+/**
+ * TODO:
+ * * processRawInputReturnSum('') -> 0
+ * * processRawInputReturnSum('1') -> 1
+ * * processRawInputReturnSum('abc') -> 0
+ * * processRawInputReturnSum('1,2,A,4') -> 7
+ * * processRawInputReturnSum('1\n2,3') -> 6
+ * * processRawInputReturnSum('//;\n1;2;3;ref') -> 6
+ */
+describe('Kata Calculator', () => {
+  const calculator = new Calculator()
+  const defaultResult = 0
+
+  it('Should return default result if is empty raw input', () => {
+    expect(calculator.processRawInputReturnSum('')).toBe(defaultResult)
+  })
+  it('Should return formated input is a single number in raw input', () => {
+    expect(calculator.processRawInputReturnSum('1')).toBe(1)
+    expect(calculator.processRawInputReturnSum('2')).toBe(2)
+  })
+  it('Should return a default result if the raw input is not a number', () => {
+    expect(calculator.processRawInputReturnSum('a')).toBe(defaultResult)
+    expect(calculator.processRawInputReturnSum('abc')).toBe(defaultResult)
+  })
+  it('Should return a sum of the raw input numbers', () => {
+    expect(calculator.processRawInputReturnSum('1,2')).toBe(3)
+    expect(calculator.processRawInputReturnSum('1,2,3')).toBe(6)
+    expect(calculator.processRawInputReturnSum('1,2,3,4')).toBe(10)
+    expect(calculator.processRawInputReturnSum('1,2,A,4')).toBe(7)
+  })
+  it('Should sepparate by default the raw input with line break or comma', () => {
+    expect(calculator.processRawInputReturnSum('1\n2,3')).toBe(6)
+    expect(calculator.processRawInputReturnSum('1\n2\n3')).toBe(6)
+    expect(calculator.processRawInputReturnSum('1\n2,a')).toBe(3)
+  })
+  it('Should sepparate the raw input with custom delimiters', () => {
+    expect(calculator.processRawInputReturnSum('//;\n1;2;3;ref')).toBe(6)
+    expect(calculator.processRawInputReturnSum('///\n2/3/5')).toBe(10)
+    expect(calculator.processRawInputReturnSum('//*\n2*2*2')).toBe(6)
+    expect(calculator.processRawInputReturnSum('//^\n2^2^2')).toBe(6)
+    expect(calculator.processRawInputReturnSum('//f\n2f3f2')).toBe(7)
+  })
+  it('Should throw an error if a negative number is found', () => {
+    expect(() => calculator.processRawInputReturnSum('-1,2,3')).toThrowError('Negatives not allowed')
+    expect(() => calculator.processRawInputReturnSum('-1,2,-3')).toThrowError('Negatives not allowed')
+  })
+  it('Should ignores numbers over than 1000', () => {
+    expect(calculator.processRawInputReturnSum('100,1002,1')).toBe(101)
+    expect(calculator.processRawInputReturnSum('1,5,1001')).toBe(6)
+  })
+  it('Should sepparate the raw input with customs delimiters', () => {
+    expect(calculator.processRawInputReturnSum('//[;][,]\n1;2,3')).toBe(6)
+    expect(calculator.processRawInputReturnSum('//[*][^]\n2*2^2')).toBe(6)
+    expect(calculator.processRawInputReturnSum('//[f][g]\n2f3g2')).toBe(7)
+  })
+})
