fb61a3c3c97630a1703898ee3df025afc9094fa9
Leto <leto4100@gmail.com>
2022-04-06 11:50:38 +0100

    Refactor

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,18 +1,25 @@
 export class Calculator {
   public processRawInputReturnSum(rawInput: string): number {
-    const firstDelimiter = rawInput.indexOf(",");
-    if(firstDelimiter === -1) {
-      const parsedNumber = parseInt(rawInput);
-      if (isNaN(parsedNumber)) return 0;
-      return parsedNumber;
-    }
-    const firstNumber = rawInput.substring(0, firstDelimiter);
-    const parsedNumber = parseInt(firstNumber);
-    if (isNaN(parsedNumber)) return 0;
-    return (
-      parsedNumber +
-      this.processRawInputReturnSum(rawInput.substring(firstDelimiter + 1))
-    );
-   
+    const [firstNumber, rest] = this.splitFirstNumber(rawInput);
+    if (rest === "") return firstNumber;
+    return firstNumber + this.processRawInputReturnSum(rest);
   }
+
+  private splitFirstNumber(input: string): [number, string] {
+    const firstDelimiter = input.indexOf(",");
+    if(firstDelimiter === -1) {
+      let result = this.parseNumber(input);
+      return [result, ""];
+    }
+    const firstNumber = this.parseNumber(input.substring(0, firstDelimiter));
+    const rest = input.substring(firstDelimiter + 1);
+    return [firstNumber, rest];
+  }
+
+  private parseNumber (number: string){
+    const parsedNumber = parseInt(number);
+    if (isNaN(parsedNumber)) return 0;
+    return parsedNumber;
+  }
+
 }
