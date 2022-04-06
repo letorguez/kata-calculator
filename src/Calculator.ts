bb23b053386ed7810d9a08ba2bec3844e78a4dca
Leto <leto4100@gmail.com>
2022-04-06 12:53:00 +0100

    Test passed: "sum the numbers separated by custom delimiters"

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -1,13 +1,21 @@
 export class Calculator {
+  readonly defaultDelimiter = /,|\n/;
+  private delimiter: RegExp;
+
+  constructor() {
+    this.delimiter = this.defaultDelimiter;
+  }
+
   public processRawInputReturnSum(rawInput: string): number {
-    const [firstNumber, rest] = this.splitFirstNumber(rawInput);
+    const input = this.processInput(rawInput);
+    const [firstNumber, rest] = this.splitFirstNumber(input);
     if (rest === "") return firstNumber;
     return firstNumber + this.processRawInputReturnSum(rest);
   }
 
   private splitFirstNumber(input: string): [number, string] {
     const firstDelimiter = this.getFirstDelimiterIndex(input);
-    if(firstDelimiter === -1) {
+    if (firstDelimiter === -1) {
       let result = this.parseNumber(input);
       return [result, ""];
     }
@@ -16,14 +24,26 @@ export class Calculator {
     return [firstNumber, rest];
   }
 
-  private parseNumber (number: string){
+  private parseNumber(number: string) {
     const parsedNumber = parseInt(number);
     if (isNaN(parsedNumber)) return 0;
     return parsedNumber;
   }
 
-  private getFirstDelimiterIndex(input:string): number {
-    const delimiter = /,|\n/;
-    return input.search(delimiter);
+  private getFirstDelimiterIndex(input: string): number {
+    return input.search(this.delimiter);
   }
+  
+  private processInput(input: string): string {
+    const delimiter = input.match(/^\/\/(.*?)\n/);
+    if (delimiter) {
+      this.delimiter = new RegExp(delimiter[1]);
+      input = input.substring(input.indexOf("\n") + 1);
+    }
+    return input;
+  }
+  // "//;\n1;2" should return "3"
+  // "//|\n1|2|3" should return "6"
+  // "//sep\n2sep3" should return "5"
+  // "//|\n1|2,3" is invalid and should return the message "'|' expected but ',' found at position 3."
 }
